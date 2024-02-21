a5ff7e73619ad3cb38992bfee967a80d259c4bf8
Anders Jensen <apjensen@ncsu.edu>
2024-02-21 15:29:00 -0500

    loads ticker

--- Main.py
+++ Main.py
@@ -7,12 +7,11 @@ import time
 import matplotlib.pyplot as plt
 import yfinance as yf
 from datetime import datetime, timedelta
-from blessed import Terminal
+from tabulate import tabulate
 
 # Initialize terminal
 TERMINAL_WIDTH = 100
-TERMINAL_START_HEIGHT = 15
-COMMAND_LIST = ["exit", "close", "load"]
+COMMAND_LIST = ["exit", "close", "equity"]
 
 
 def interpret_command(command: list) -> str:
@@ -40,26 +39,37 @@ def execute_command(command: list) -> list:
         list: row_data[] from executing given ncommand
     """
     match command[0]:
-        case "load":
-            return load_command(command)
+        case "equity":
+            return equity_command(command)
         case _:
             return f'{f'Unexpected Exception':^{TERMINAL_WIDTH - 4}}'
             
-def load_command(command) -> list:
+def equity_command(command: list) -> list:
     """Loads a symbol main menu for commands
 
     Args:
-        command (_type_): _description_
+        command (list): _description_
 
     Returns:
         list: _description_
     """
     row_data = []
     if (len(command) != 2):
-        return [(f'{f'Invalid Arguments -> Use load <symbol>':^{TERMINAL_WIDTH - 4}}')]
+        return [(f'{f'Invalid Arguments -> Use equity <symbol>':^{TERMINAL_WIDTH - 4}}')]
     row_data.append(f'{f'<{command[1].upper()}>':^{TERMINAL_WIDTH - 4}}')
     row_data.append((" " * (TERMINAL_WIDTH - 4)))
+    
+    ticker = yf.Ticker(command[1])
+    price = ticker.history(period='1d').iloc[-1].Close
+    row_data.append(f'{f'${price:.4f}':^{TERMINAL_WIDTH - 4}}')
     return row_data
+
+#    except Exception as e:
+#        row_data.append(f'{f'<{type(e).__name__}> Exception':^{TERMINAL_WIDTH - 4}}')
+#        row_data.append(f'{f'<{command[1].upper()}> is not a valid symbol.':^{TERMINAL_WIDTH - 4}}')
+#        row_data.append((" " * (TERMINAL_WIDTH - 4)))
+#        return row_data
+                    
                     
 def print_logo() -> None:
     print("▄▄███▄▄·██╗   ██╗ ██████╗ ██╗     \n" +
@@ -121,8 +131,12 @@ def main() -> None:
     # Commands
     print_logo()
     row_data = []
-    for i in range(0, TERMINAL_START_HEIGHT):
-        row_data.append(("*" + " "*(TERMINAL_WIDTH - 5)))
+    for i in range(0, 5):
+        row_data.append(("*" + " "*(TERMINAL_WIDTH - 6) +"*"))
+    row_data.append(("COMMANDS:" + " "*(TERMINAL_WIDTH - 14) +"*"))
+    row_data.append(("close - closes terminal" + " "*(TERMINAL_WIDTH - 28) +"*"))
+    row_data.append(("exit - return to home (this page)" + " "*(TERMINAL_WIDTH - 38) +"*"))
+    row_data.append(("equity <symbol> - loads equity" + " "*(TERMINAL_WIDTH - 35) +"*"))
 
     while True:
         window_main(row_data)
